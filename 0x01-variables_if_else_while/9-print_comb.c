fe35773ab40e04da674c725fd961148eacfc1e63
CaroMusangi1 <kitongacarol8@gmail.com>
2023-07-14 11:42:33 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h> 
-/**
- * main - A program that prints combination of all two digit numbers.
- * 
+/** 
+ * main - A program that prints combination of all one digit numbers.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 
-        int num = 0;	
-        int tens;
-        int ones; 
+        int num;
 
-        while (num <= 99)
+        for (num = 0; num <= 9; num++)
         {
-                tens = num % 10;
-                ones = num / 10; 
-
-                if (ones < tens)
+                putchar(num + '0');
+                if (num != 9)
                 {
-                        putchar(ones + '0');
-                        putchar(tens + '0');
-                        if (num < 89)
-                        {
-                                putchar(',');
-                                putchar(' '); 
-                        }
-                }
-                num++;
+                        putchar(',');
+                        putchar(' ');
+                } 
         }
-        putchar('\n');
 
-        return (0);
+putchar('\n');
+
+return (0);
 }
