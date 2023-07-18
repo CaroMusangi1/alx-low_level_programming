c78df9a6bfeafd4a1786384c89be7bfb6a728de2
CaroMusangi1 <kitongacarol8@gmail.com>
2023-07-18 10:51:34 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,51 @@
-#include "main.h"
-/**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
+#include <stdio.h>  // Include the standard input/output library
+
+void print_times_table(int n);
+
+int main()
+{
+    int n = 10;  // Example: Printing times table up to 10
+    print_times_table(n);
+
+    return 0;
+}
+
 void print_times_table(int n)
 {
-	int i, j, k;
-	
-	if (n >= 0 && n <= 15)
-		{
-			for (i = 0; i <= n++;)
-			{
-				for (j = 0; j <= n; j++)
-				{
+    int i, j, k;
 
-                                k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-                                        _putchar(' ');
-                                        _putchar(k + '0');
-                                } else if (k >= 10 && k < 100)
-                                {
-                                        _putchar(',');
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                        _putchar((k / 10) + '0');
-                                        _putchar((k % 10) + '0');
-                                } else if (k >= 100)
-                                {
-                                        _putchar(',');
-                                        _putchar(' ');
-                                        _putchar((k / 100) + '0');
-                                        _putchar(((k / 10) % 10) + '0');
-                                        _putchar((k % 10) + '0');
-                                }
-                        }
-                        _putchar('\n');
+    if (n >= 0 && n <= 15)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                k = j * i;
+
+                if (j == 0)
+                {
+                    printf("%d", k);
                 }
+                else
+                {
+                    printf(", ");
+
+                    if (k < 10)
+                    {
+                        printf("   %d", k);
+                    }
+                    else if (k >= 10 && k < 100)
+                    {
+                        printf("  %d", k);
+                    }
+                    else if (k >= 100)
+                    {
+                        printf(" %d", k);
+                    }
+                }
+            }
+            printf("\n");
         }
+    }
 }
+
